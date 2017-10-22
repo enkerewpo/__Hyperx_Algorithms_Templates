56510b9a08a9c3376131a15f4649069d4e536c46
Kvar_ispw17 <enkerewpo@gmail.com>
2017-10-22 21:40:35 +0800

    Update __Hyperx_Quick_Long_Long_Multiply.cpp

--- __Hyperx_Quick_Long_Long_Multiply.cpp
+++ __Hyperx_Quick_Long_Long_Multiply.cpp
@@ -1,5 +1,5 @@
+#define EPS 1e-8
 long long mul(long long a, long long b, long long p) {
-    long long ret = (a * b - (long long)((long double)a / p * b) * p) % p;
-    if(ret < 0) ret += p;
-    return ret;
+    long long ret = (a * b - (long long)((long double)a / p * b + EPS) * p);
+    return ret < 0 ? ret + p : ret;
 }
